37eaa3632f48a11866f49bad8c284f12c09facfb
Al Tsai[:atsai] <atsai@mozilla.com>
2014-02-12 14:48:05 +0800

    updated

--- flash_tarako_eng.sh
+++ flash_tarako_eng.sh
@@ -1,13 +1,7 @@
-rm -rf b2g-distro
-rm B2G.v1.3.0.Tarako.eng.zip
+rm gaia.zip
+rm b2g-28.0.en-US.android-arm.tar.gz
 
-wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/B2G.v1.3.0.Tarako.eng/lastSuccessfulBuild/artifact/out/B2G.v1.3.0.Tarako.eng.zip
+wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/B2G.v1.3.0.Tarako.v1.3t/lastSuccessfulBuild/artifact/out/gaia.zip
+wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/B2G.v1.3.0.Tarako.v1.3t/lastSuccessfulBuild/artifact/objdir-gecko/dist/b2g-28.0.en-US.android-arm.tar.gz
 
-unzip B2G.v1.3.0.Tarako.eng.zip
-
-adb reboot-bootloader
-fastboot flash boot b2g-distro/out/target/product/*/boot.img
-fastboot flash system b2g-distro/out/target/product/*/system.img
-fastboot flash userdata b2g-distro/out/target/product/*/userdata.img
-
-fastboot reboot
+./shallow_flash.sh -ggaia.zip -Gb2g-28.0.en-US.android-arm.tar.gz -f
