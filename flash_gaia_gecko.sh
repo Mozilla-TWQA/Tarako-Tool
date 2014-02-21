5a81d6d43da3b79986b5fda15a2a1dfee0fa198c
askeing <askeing@gmail.com>
2014-02-21 10:32:24 +0800

    Updated shallow flash option from -f to -y

--- flash_gaia_gecko.sh
+++ flash_gaia_gecko.sh
@@ -4,4 +4,4 @@ rm b2g-28.0.en-US.android-arm.tar.gz
 wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/B2G.v1.3.0.Tarako.v1.3t/lastSuccessfulBuild/artifact/out/gaia.zip
 wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/B2G.v1.3.0.Tarako.v1.3t/lastSuccessfulBuild/artifact/objdir-gecko/dist/b2g-28.0.en-US.android-arm.tar.gz
 
-./shallow_flash.sh -ggaia.zip -Gb2g-28.0.en-US.android-arm.tar.gz -f
+./shallow_flash.sh -ggaia.zip -Gb2g-28.0.en-US.android-arm.tar.gz -y
