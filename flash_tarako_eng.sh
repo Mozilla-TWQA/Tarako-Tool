ce3246668898da7d5f2ecbab6a2153b686942385
askeing <askeing@gmail.com>
2014-03-20 17:42:36 +0800

    Update flash eng build script

--- flash_tarako_eng.sh
+++ flash_tarako_eng.sh
@@ -1,9 +1,11 @@
+TARGET_BUILD="B2G.v1.3.0.Tarako.eng"
+
 rm -rf b2g-distro
-rm B2G.v1.3.0.Tarako.eng.zip
+rm ${TARGET_BUILD}.zip
 
-wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/B2G.v1.3.0.Tarako.eng/lastSuccessfulBuild/artifact/out/B2G.v1.3.0.Tarako.eng.zip
+wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/${TARGET_BUILD}/lastSuccessfulBuild/artifact/out/${TARGET_BUILD}.zip
 
-unzip B2G.v1.3.0.Tarako.eng.zip
+unzip ${TARGET_BUILD}.zip
 
 adb reboot-bootloader
 fastboot flash boot b2g-distro/out/target/product/*/boot.img
