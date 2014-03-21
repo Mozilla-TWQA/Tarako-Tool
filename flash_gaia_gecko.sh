b6e55cbc20fa7bb353206ce846bb338687a9582b
Hubert Figuière <hub@figuiere.net>
2014-03-21 14:19:10 -0400

    Check for errors and return codes.

--- flash_gaia_gecko.sh
+++ flash_gaia_gecko.sh
@@ -2,6 +2,22 @@ rm gaia.zip
 rm b2g-28.0.en-US.android-arm.tar.gz
 
 wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/B2G.v1.3.0.Tarako.v1.3t/lastSuccessfulBuild/artifact/out/gaia.zip
+
+if [ $? -ne 0 ] ; then
+    echo "$0: wget error" >&2
+    exit 1
+fi
+
 wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/B2G.v1.3.0.Tarako.v1.3t/lastSuccessfulBuild/artifact/objdir-gecko/dist/b2g-28.0.en-US.android-arm.tar.gz
+
+if [ $? -ne 0 ] ; then
+    echo "$0: wget error" >&2
+    exit 1
+fi
+
+if [ ! -f gaia.zip -o ! -f b2g-28.0.en-US.android-arm.tar.gz ] ; then
+    echo "$0: File not found." >&2
+    exit 2
+fi
 
 ./shallow_flash.sh -ggaia.zip -Gb2g-28.0.en-US.android-arm.tar.gz -y
--- flash_tarako_eng.sh
+++ flash_tarako_eng.sh
@@ -5,7 +5,22 @@ rm ${TARGET_BUILD}.zip
 
 wget http://release1-qa.corp.tpe1.mozilla.com:8080/job/${TARGET_BUILD}/lastSuccessfulBuild/artifact/out/${TARGET_BUILD}.zip
 
+if [ $? -ne 0 ] ; then
+    echo "$0: wget error" >&2
+    exit 1
+fi
+
+if [ ! -f ${TARGET_BUILD}.zip ] ; then
+    echo "$0: File not found." >&2
+    exit 2
+fi
+
 unzip ${TARGET_BUILD}.zip
+
+if [ $? -ne 0 ]; then
+    echo "$0: unzip error." >&2
+    exit 3
+fi
 
 adb reboot-bootloader
 fastboot flash boot b2g-distro/out/target/product/*/boot.img
